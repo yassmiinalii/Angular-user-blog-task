951893b2af00cad73b5e9b23fa57d68ec235d668
yassmiinalii <yasmiiinaliii@gmail.com>
2023-08-19 04:20:32 +0300

    init home page style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
+  {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
 
 ];
 
