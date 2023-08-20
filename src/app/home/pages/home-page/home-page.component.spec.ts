0cc30e1d30ab1ecbea114206b56399d284ca9b24
yassmiinalii <yasmiiinaliii@gmail.com>
2023-08-20 07:53:24 +0300

    Add unit test to home page component

--- src/app/home/pages/home-page/home-page.component.spec.ts
+++ src/app/home/pages/home-page/home-page.component.spec.ts
@@ -1,23 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, of } from 'rxjs';
 
+import { StoreService } from 'src/app/core/store/store.service';
 import { HomePageComponent } from './home-page.component';
+import { UsersComponent } from '../../components/users-navbar/users.component';
+import { PostListComponent } from '../../components/post-list/post-list.component';
+import { PostSkeltonLoaderComponent } from '../../components/post-list/post-card/post-skelton-loader/post-skelton-loader.component';
+import { SkeltonLoaderComponent } from 'src/app/shared/components/skelton-loader/skelton-loader.component';
+import { By } from '@angular/platform-browser';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let storeServiceSpy: jasmine.SpyObj<StoreService>;
+  let activatedRouteStub: Partial<ActivatedRoute>;
 
   beforeEach(async () => {
+    const storeSpy = jasmine.createSpyObj('StoreService', ['init']);
+    const queryParamSubject = new BehaviorSubject<{ userId: number }>({
+      userId: 1,
+    });
+    activatedRouteStub = {
+      queryParams: queryParamSubject.asObservable(),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [
+        HomePageComponent,
+        UsersComponent,
+        PostListComponent,
+        PostSkeltonLoaderComponent,
+        SkeltonLoaderComponent,
+      ],
+      providers: [
+        { provide: StoreService, useValue: storeSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteStub },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
+    storeServiceSpy = TestBed.inject(
+      StoreService
+    ) as jasmine.SpyObj<StoreService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render post list based on query parameter', () => {
+    const queryParam = { userId: 1 };
+    activatedRouteStub.queryParams = of(queryParam);
+
+    fixture.detectChanges();
+
+    const postListElement = fixture.debugElement.query(By.css('app-post-list'));
+    expect(postListElement).toBeTruthy();
+  });
+
+  it('should initialize store on init', () => {
+    fixture.detectChanges();
+    expect(storeServiceSpy.init).toHaveBeenCalled();
+  });
+
+
 });
--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
 import { StoreService } from 'src/app/core/store/store.service';
 
 @Component({
@@ -10,26 +9,12 @@ import { StoreService } from 'src/app/core/store/store.service';
 })
 export class HomePageComponent  implements OnInit {
 
+  queryParam$ = this.route.queryParams;
+
   constructor(private stor: StoreService , private route : ActivatedRoute){}
+
   ngOnInit(): void {
     this.stor.init()
   }
-
-  queryParam$ = this.route.queryParams
-  // .pipe(
-  //   switchMap(queryParams => {
-  //     //check If there userId queryParam
-  //     if(queryParams?.['userId']){
-  //       //if it return a new subscribe to get posts with handling error
-  //       // const userId = +queryParams?.['userId'];
-  //       // return this.apiServices.getPostsByUserId(userId).pipe(
-  //       //   catchError(error => {
-  //       //      this.error = 'An error occurred while fetching posts!.'
-  //       //     return of();
-  //       //   }))
-  //     }
-  //     return []
-  //   })
-  // )
 
 }
